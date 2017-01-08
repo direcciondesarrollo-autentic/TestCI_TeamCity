c2482db6a3496d1cee88d72528ab7192622cae5b
Andrés ardila <direcciondesarrollo@autentic.com.co>
2017-01-08 17:48:03 -0500

    ..

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -10,7 +10,7 @@ public class CalculatorTest {
     public void multiplicationOfZeroIntegersShouldReturnZero() {
         Calculator calculator = new Calculator();
 
-        //assert statements  //
+        //assert statements
         assertEquals("10 + 2 must be 12", 12, calculator.sum(10, 2));
         assertEquals("11 - 5 must be 0", 6, calculator.res(11, 5));
     }
