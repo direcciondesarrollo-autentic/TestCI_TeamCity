c91b865dc0a86caf7d6ee39d77e90067f6a9bfad
Andrés ardila <direcciondesarrollo@autentic.com.co>
2017-01-08 17:24:17 -0500

    ..

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,77 @@
+import java.util.Scanner;
+
 /**
  * Created by root on 1/8/17.
  */
 public class Calculator {
+
+    public int sum(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public int res(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static void main(String[] args) {
+
+        int[] numeros;
+
+        numeros = new int[2];
+
+        int operacion;
+        int numero1;
+        int numero2;
+        int suma;
+        int resta;
+        int multiplicacion;
+        int division;
+        int desicion;
+
+        Scanner buffer = new Scanner(System.in);
+        do {
+            do {
+                System.out.print("Ingrese un opcionn");
+                System.out.print("opcion 1:Suman");
+                System.out.print("opcion 2:Restan");
+                System.out.print("opcion 3:Multiplicacionn");
+                ;
+                System.out.print("opcion 4ivisionn");
+                operacion = buffer.nextInt();
+                if (operacion > 4 || operacion < 1) {
+                    System.out.println("Ingrese una opcion valida");
+                }
+            } while (operacion > 4 || operacion < 1);
+            for (int i = 0; i < numeros.length; i++) {
+                System.out.println("Ingrese numero:" + (1 + i));
+                if (i == 0) {
+                    numero1 = buffer.nextInt();
+                    numeros[0] = numero1;
+                }
+                if (i == 1) {
+                    numero2 = buffer.nextInt();
+                    numeros[1] = numero2;
+                }
+            }
+            if (operacion == 1) {
+                suma = numeros[0] + numeros[1];
+                System.out.println("la suma es de:" + suma);
+            }
+            if (operacion == 2) {
+                resta = numeros[0] - numeros[1];
+                System.out.println("la resta es de:" + resta);
+            }
+            if (operacion == 3) {
+                multiplicacion = numeros[0] * numeros[1];
+                System.out.println("la multipliacion es de:" + multiplicacion);
+            }
+            if (operacion == 4) {
+                division = numeros[0] / numeros[1];
+                System.out.println("la division es de:" + division);
+            }
+            System.out.println("ingrese '1' si quiere realizar otra operacion '2' si quiere cerrar el programa");
+            desicion = buffer.nextInt();
+        } while (desicion == 1 && desicion != 2);
+    }
+
 }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by root on 1/8/17.
+ */
+public class CalculatorTest {
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        Calculator calculator = new Calculator();
+
+        //assert statements
+        assertEquals("10 + 2 must be 12", 12, calculator.sum(10, 2));
+        assertEquals("11 - 5 must be 0", 6, calculator.res(11, 5));
+    }
+}
